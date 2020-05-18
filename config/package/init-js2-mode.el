6188dfa7d9bf04b2b060545b831a4d740646e938
liangjunming <allenprivate@163.com>
2020-05-18 19:55:28 +0800

    add packege typescript-mode

--- config/package/init-js2-mode.el
+++ config/package/init-js2-mode.el
@@ -2,12 +2,20 @@
 ;; To create a file, visit it with <open> and enter text in its buffer.
 
 (use-package js2-mode
-  :ensure t)
-
-(setq auto-mode-alist
+  :ensure t
+  :config
+  (setq auto-mode-alist
       (append
        '(("\\.js\\'" . js2-mode))
-       auto-mode-alist))
+       auto-mode-alist)))
+
+(use-package typescript-mode
+  :ensure t
+  :config
+  (setq auto-mode-alist
+      (append
+       '(("\\.ts\\'" . typescript-mode))
+       auto-mode-alist)))
 
 
 (provide 'init-js2-mode)
